a0671abb80ca1a0c7ec99ddb8e60effa7cc273a7
Nicolas Constante Guedert <nicolascguedert@pop-os.localdomain>
2023-03-10 09:54:09 -0300

    remoção print

--- membro1.py
+++ membro1.py
@@ -1,4 +1,3 @@
 def hello():
 	return "\nOlá, eu sou o Nicolas Constante Guedert, 19 anos. Tenho como hobbies a patinação e vizualização de streams. Não possuo experiência prévia ao ingresso na universidade, estou desenvolvendo-a no decorrer do curso. Espero da disciplina um grande desenvolvimento no pensamento lógico como programador e grande carga de conhecimento acerca de como ser um bom profissional." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
 
-print(hello())
